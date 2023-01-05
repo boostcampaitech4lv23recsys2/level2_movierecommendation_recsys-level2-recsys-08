74b2dd71227d53bd76c6e5996bc9b7d6e38286ec
netsus <cwjcsk@naver.com>
2023-01-05 20:25:04 +0900

    [Feat][조원준] a파일의 크기가 클때도 되도록 기능 수정

--- utils/similarity.py
+++ utils/similarity.py
@@ -7,7 +7,7 @@ warnings.filterwarnings('ignore')
 def main(args):
     df1 = pd.read_csv(args.a)
     df2 = pd.read_csv(args.b)
-    if len(df1)!=len(df2):
+    if len(df1)<len(df2):
         print(f"{args.a}와 {args.b}의 길이가 같아야 합니다.")
     else:
         sim=df1.merge(df2, on=['user','item'])
@@ -17,7 +17,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="-a 와 -b 파일의 유사도를 출력합니다.")
-    parser.add_argument("-a", default = "/opt/ml/input/code/Recbole/EASE_1_0_Top20_remove_review_after_movie.csv", type=str)
+    parser.add_argument("-a", default = "/opt/ml/input/code/Ensemble/EASE_1--FFM_0.6--NeuMF_0.6--RecVAE_0.9.csv", type=str)
     parser.add_argument("-b", required=True, type=str)
     args = parser.parse_args()
     main(args)
